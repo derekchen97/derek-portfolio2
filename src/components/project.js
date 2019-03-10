4b59ae10f563e83b14563c6ebfc5eda413873f0f
Derek Chen <dezzychen@googlemail.com>
2019-03-10 17:13:49 +1100

    added project cards and a flexbox to contain them

--- src/components/project.js
+++ src/components/project.js
@@ -26,11 +26,6 @@ class Project extends Component {
       minWidth: "450",
       margin: "auto"
     };
-    this.cardTitleStyle = {
-      color: "#000",
-      height: "176px",
-      background: `url(${require("../assets/Java Slick Game Image.png")})`
-    };
   }
 
   //varialbes here
@@ -38,23 +33,29 @@ class Project extends Component {
   /**
    * Checks the state of the tabs (if it has been switched)
    * If it has changed, show the appropriate content
+   *
+   * Insert all the projects in toggle categories
    */
-  toggleCategories() {
+  insertProjects() {
     if (this.state.activeTab === 0) {
       //University project (tabId = 0 is index of the first tab)
       return (
-        <Card shadow={5} style={this.cardStyle}>
-          <CardTitle style={this.cardTitleStyle}>Java Slick Game</CardTitle>
-          <CardText>lorem test test</CardText>
-          <CardActions>
-            <Button colored>Github</Button>
-            <Button colored>demo</Button>
-            <Button colored>about</Button>
-          </CardActions>
-          <CardMenu style={{ color: "#fff" }}>
-            <IconButton name="share" />
-          </CardMenu>
-        </Card>
+        <div className="projects-grid">
+          {/* project 1 - slick game in java */}
+          {this.createProject(
+            require("../assets/javaslickgame.png"),
+            "Java Slick Game",
+            "Using principles of Object Oriented programming and UML diagrams to create a 2d RPG game using the SLICK + LWJGL (graphic rendering) libraries",
+            ["GitHub", "Video", "Demo"]
+          )}
+          {/* project 2 - Unity 3d game in c# */}
+          {this.createProject(
+            require("../assets/javaslickgame.png"),
+            "Unity 3D Game in C#",
+            "Using principles of Object Oriented programming in Unity 3D to create a 3D game with custom shaders, game persistance (save files) and 3D modelling software (Fusion 360) ",
+            ["GitHub", "Video", "Demo"]
+          )}
+        </div>
       );
     } else if (this.state.activeTab === 1) {
       //University project (tabId = 0 is index of the first tab)
@@ -74,18 +75,61 @@ class Project extends Component {
           onChange={tabId => this.setState({ activeTab: tabId })}
         >
           <Tab>University Projects</Tab>
-          <Tab>Personal Projects</Tab>
+          {/* <Tab>Personal Projects</Tab> */}
         </Tabs>
 
-        <section className="projects-grid">
-          <Grid className="projects-grid">
-            <Cell col={12}>
-              <div className="content">{this.toggleCategories()} </div>
-            </Cell>
-          </Grid>
-        </section>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.insertProjects()} </div>
+          </Cell>
+        </Grid>
       </div>
     );
+  }
+
+  //project methods down here:
+  /**
+   * This method returns a project card with the following inputs
+   * @param {*} imageImport - takes in the require/import of the image
+   * @param {*} title
+   * @param {*} text
+   * @param {*} buttons - array of strings (try keep under 3 strings)
+   */
+  createProject(imageImport, title, text, buttons) {
+    return (
+      <div>
+        <Card className="project" shadow={5} style={this.cardStyle}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "200px",
+              background: `url(${imageImport})` + "center / cover"
+
+              // backgroundSize: "cover"
+            }}
+          >
+            {title}
+          </CardTitle>
+          <CardText>{text}</CardText>
+          <CardActions>{this.createProjectButton(buttons)}</CardActions>
+          <CardMenu style={{ color: "#fff" }}>
+            {/* <IconButton name="share" /> */}
+          </CardMenu>
+        </Card>
+      </div>
+    );
+  }
+
+  /**
+   * Returns an array of button react component from a loop of button string names
+   * @param {*} buttons - a array of strings for the button name
+   */
+  createProjectButton(buttons) {
+    let items = [];
+    buttons.forEach(button => {
+      items.push(<Button colored>{button}</Button>);
+    });
+    return items;
   }
 }
 
